b48517fc457e192cad9b4305b28f59d300c84d86
marthebull <marthebullpettersen@gmail.com>
2023-05-01 02:38:13 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <h3>hello</h3>
+      <h4>hello</h4>
+      <h5>hello</h5>
+      <h5>hello</h5>
+      <a>hello</a>
+      <a>hello</a>
+      <p>hello <a>hello</a> hello</p>
+      <a>hello</a>
+      <p>hello</p>
+      <button className="button-outline-dark">hello</button>
+      <label>hello</label>
+      <input aria-label="hello" placeholder="hello"></input>
+      <div className="flex-h">
+      <img className="icon"
+     src="https://raw.githubusercontent.com/marthebull/holidaze-exam2023/187f416b0fd1cc2255c1e2d534bf8e89b5ee8657/images/people-icon.svg"
+     alt="Grapefruit slice atop a pile of other slices"></img>
+     <p>hello</p>
+      </div>
+      <div className="flex-h">
+      <img className="icon"
+     src="https://raw.githubusercontent.com/marthebull/holidaze-exam2023/187f416b0fd1cc2255c1e2d534bf8e89b5ee8657/images/moon-sea-icon.svg"
+     alt="Grapefruit slice atop a pile of other slices"></img>
+     <p>hello</p>
+      </div>
+      <div className="flex-h">
+      <img className="icon"
+     src="https://raw.githubusercontent.com/marthebull/holidaze-exam2023/187f416b0fd1cc2255c1e2d534bf8e89b5ee8657/images/coffe-cup-icon.svg"
+     alt="Grapefruit slice atop a pile of other slices"></img>
+     <p>hello</p>
+      </div>
+      <div className="flex-h"><input type="checkbox"></input><p>hello</p></div>
+      <input type="checkbox"></input>
+      
+      
+    </div>
+  );
+}
+
+export default App;
