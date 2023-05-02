fd992ff570a066ec0567001d333bba2fea655e0e
marthebull <marthebullpettersen@gmail.com>
2023-05-02 02:12:08 +0200

    Update styles.js

--- src/components/layout/navbar/right-menu/styles.js
+++ src/components/layout/navbar/right-menu/styles.js
@@ -16,10 +16,11 @@ export const RightNavMenu = styled.ul`
   justify-content: start;
   z-index: 10;
   background-color: white;
-  box-shadow: -4px -3px 10px 5px rgba(0, 0, 0, 0.05);
+  box-shadow: -2px -3px 5px 5px rgba(0, 0, 0, 0.03);
 
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(105%)")};
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(103%)")};
   transition: transform 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
 
   @media screen and (min-width: 901px) {
     display: none;
